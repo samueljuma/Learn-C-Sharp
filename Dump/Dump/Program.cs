948ee78377fa8361c717ebbc9ecf480768ad9b13
samueljuma <samueljuma1024@gmail.com>
2022-01-03 06:13:39 +0300

    variable declarion and assignment

---
+++ Dump/Dump/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Dump {
+    public static class Program {
+        static void Main(string[] args) {
+            // Variables Declaration and Assignment
+            int number = 12;
+            string name = "Samuel Juma";
+            bool isGood = false;
+            double avarage = 20.02;
+
+            Console.WriteLine("\nisGood: "+isGood + "\nname: "+name + "\nnumber: " + number);
+        }
+    }
+}
