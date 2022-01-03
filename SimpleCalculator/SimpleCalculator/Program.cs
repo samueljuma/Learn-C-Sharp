f4c3a122122af53b50ca90329cdcbf7953a930f1
samueljuma <samueljuma1024@gmail.com>
2022-01-03 06:37:35 +0300

    simple calculator 1.0

---
+++ SimpleCalculator/SimpleCalculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Dump
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //simple calculator
+            int num1, num2;
+            Console.Write("Enter first number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(num1 + " + " + num2 + " = " + (num1 + num2));
+
+        }
+    }
+}
