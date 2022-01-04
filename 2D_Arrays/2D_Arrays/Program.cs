8abc41cc5f679d9673a04e1a706fe7d9ee56ea55
samueljuma <samueljuma1024@gmail.com>
2022-01-04 11:56:13 +0300

    2D Arrays

---
+++ 2D_Arrays/2D_Arrays/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Arrays_2D{
+    public class Program {
+        static void Main(string[] args) {
+            int[,] a =
+            {
+                { 1, 2 },
+                { 3, 4 },
+                { 4, 5 },
+            };
+
+            int[,] b = new int[3,5];
+            b[0,0] = 100;
+
+
+            Console.WriteLine(a[0,0]);
+            Console.WriteLine(b[0,0]);
+        }
+    }
+}
