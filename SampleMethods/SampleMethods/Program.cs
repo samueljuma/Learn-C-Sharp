2c95fdee4c774a0c59ded245c9fdd0949bac9558
samueljuma <samueljuma1024@gmail.com>
2022-01-04 09:59:42 +0300

    Methods 101

---
+++ SampleMethods/SampleMethods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SampleMethods {
+    public class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("*** Executing SayHi()****\n");
+            SayHi();
+            Console.WriteLine("*** Executing SayHi(args)****\n");
+            SayHi("Samuel");
+        }
+        //Method with No argument 
+        static void SayHi() {
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi "+ name +"\n");
+        }
+
+        //Method with Argument
+        static void SayHi(string name) { 
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
