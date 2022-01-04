4ef0c9dd948f1c9d0a37ea2f477d0b67cd183e7d
samueljuma <samueljuma1024@gmail.com>
2022-01-04 13:10:34 +0300

    classes and objects 101

---
+++ ClassesAndObjects/ClassesAndObjects/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public string ISBN_Number;
+        public string description;
+        
+    }
+}
---
+++ ClassesAndObjects/ClassesAndObjects/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args) { 
+            Book book = new Book();
+            book.author = "Juma";
+
+            Console.WriteLine(book.title);
+        }
+    }
+}
