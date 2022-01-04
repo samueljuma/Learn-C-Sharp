95901ff5838cd66f08413e155e4c3ff1406dcc74
samueljuma <samueljuma1024@gmail.com>
2022-01-04 14:45:08 +0300

    getters and setters 101

---
+++ GettersAndSetters/GettersAndSetters/Movie.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettersAndSetters
+{
+    class Movie
+    {
+        private string title;
+        private string director;
+        private string rating;
+
+        public Movie(string aTitle, string aDirector, string aRating) { 
+            title = aTitle;
+            director = aDirector;
+            Rating = aRating;
+        }
+
+        // SETTERS and GETTERS
+        public string Rating {
+            get { return rating; }
+            set {
+                string[] ratings= { "NR", "PG", "PG-13", "G" };
+                rating = (ratings.Contains(value)) ? value : "NR";             
+            }
+        }
+
+        override public string ToString() {
+            return "Title: " + title + ", Director: " + director + ", Rating: " + rating;
+        }
+
+    }
+}
---
+++ GettersAndSetters/GettersAndSetters/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GettersAndSetters {
+
+    public class Program {
+        static void Main(string[] args) {
+            Movie movie = new Movie("Blue Skies", "Edmont Reddie", "Jumma");
+
+            Console.WriteLine(movie);
+        }
+    }
+}
