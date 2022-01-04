f776f7b593248383d7ecf4ff43082362d5441f4a
samueljuma <samueljuma1024@gmail.com>
2022-01-04 11:09:06 +0300

    Loops 101

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Loops {
+    public class Program {
+        static void Main(string[] args) { 
+            int index = 0;
+            // While Loop
+           
+            while (index < 5) { 
+                Console.WriteLine("Iteration" + index);
+                index++;
+            }
+
+            // For Loop
+            for (int i = 0; i <= 5; i++) {
+                Console.WriteLine("Iteration" + i); 
+            }
+        }
+
+        
+
+        
+    }
+}
